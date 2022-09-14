b62463e88a112590025a87a9857bd024dcae651f
codeLop-6 <mahmodb29@yahoo.com>
2022-09-14 16:24:13 +0200

    v1

--- app/src/main/java/com/example/kotlin/view/fragments/HomeCycle/HomeFragment.kt
+++ app/src/main/java/com/example/kotlin/view/fragments/HomeCycle/HomeFragment.kt
@@ -1,21 +1,34 @@
 package com.example.kotlin.view.fragments.HomeCycle
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.compose.runtime.mutableStateListOf
 import com.example.kotlin.R
 import com.example.kotlin.view.fragments.BaseFragment
+import java.sql.DriverManager.println
+import java.util.*
 
 class HomeFragment : Fragment() {
-    var root: View? = null
+    private var root: View? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         root = inflater.inflate(R.layout.fragment_home, container, false)
+        main()
         return root
+
+    }
+
+    private fun main() {
+        var list= mutableStateListOf<String>()
+        list[0]="ali"
+        list[1]="noor"
+        Log.d("sddddd", "$list")
     }
 }
