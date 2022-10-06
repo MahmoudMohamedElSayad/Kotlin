163f893278c59ad93ba5afcc1a3b4d37ec511d2c
codeLop-6 <mahmodb29@yahoo.com>
2022-10-06 16:35:55 +0200

    v9

---
+++ app/src/main/java/com/example/kotlin/view/fragments/HomeCycle/OrdersFragment.kt
@@ -0,0 +1,19 @@
+package com.example.kotlin.view.fragments.HomeCycle
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kotlin.R
+import com.example.kotlin.view.fragments.BaseFragment
+
+class OrdersFragment : BaseFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_orders, container, false)
+    }
+}
