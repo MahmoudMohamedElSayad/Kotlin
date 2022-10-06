e18d97b5d07f57521153d85076a2f6e00fba9ec9
codeLop-6 <mahmodb29@yahoo.com>
2022-10-06 16:58:55 +0200

    v10

---
+++ app/src/main/java/com/example/kotlin/view/fragments/morePages/AboutUsFragment.kt
@@ -0,0 +1,21 @@
+package com.example.kotlin.view.fragments.morePages
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kotlin.R
+import com.example.kotlin.view.fragments.BaseFragment
+
+class AboutUsFragment : BaseFragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about_us, container, false)
+    }
+}
